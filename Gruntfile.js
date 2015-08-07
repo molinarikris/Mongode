5cf030ff0fb3d0b16653f0d06234d5af4350a219
molinarikris <kamolinari42@gmail.com>
2015-08-07 18:09:30 -0400

    Created test spec to use a TDD approach. Set up task runner.

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		jshint: {
+			options: {
+				node: true
+			},
+			all: {
+				src: ['Gruntfile.js', 'lib/*.js', 'tests/*.js']
+			}
+		},
+		mochaTest: {
+			test: {
+				src: ['test/*.js']
+			}
+		},
+		uglify: {
+			build: {
+				files: {'src/mongode.min.js': ["lib/*.js"]}
+			}
+		},
+		clean: {
+			build: {
+				src: ['src/mongode.min.js']
+			}
+		},
+		watch: {
+			all: {
+				files: ['lib/*.js'],
+				tasks: ['jshint', 'clean', 'uglify']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-mocha-test');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['jshint', 'mochaTest', 'clean', 'uglify', 'watch']);
+	grunt.registerTask('no-watch', ['jshint', 'mochaTest', 'clean', 'uglify']);
+};
---
+++ test/spec.js
@@ -0,0 +1,65 @@
+var assert = require('assert');
+var db = new require('../lib/index.js')('test', 'controller');
+
+describe('Mongode', function(){
+	describe('#connection', function() {
+		it('should connect without error and return non-empty database object', function(done) {
+			db.connection(function(err, db){
+				assert.equal(null, err);
+				assert.notEqual(0, Object.keys(db).length);
+				done();
+			})
+		});
+	});
+	describe('#create', function(){
+		it('should make a new database entry with no error', function(done){
+			db.create({"test": "pass", "a": 1}, function(err, res){
+				assert.equal(null, err);
+				assert.equal(1, res.result.n);
+				done();
+			});
+		});
+		it('should take an array of data and insert it...with no error', function(done){
+			db.create([{"another test": "pass"}, {"yet another test": "pass"}, {"test":"pass", "a": 2}], function(err, res){
+				assert.equal(null, err);
+				assert.equal(3, res.result.n);
+				done();
+			});
+		})
+	});
+	describe('#read', function(){
+		it('should create an array of all database entries given no query', function(done){
+			db.read(function(err, docs){
+				assert.equal(null, err);
+				assert.equal(4, docs.length);
+				done();
+			})
+		});
+		it('should create an array of matches according to a mongodb query', function(done){
+			db.read({"a":{$exists: true}}, function(err, docs){
+				assert.equal(null, err);
+				assert.equal(2, docs.length);
+				done();
+			})
+		})
+	});
+	describe('#update', function() {
+		it('should find a db entry and modify it without error', function(done){
+			db.update({"test": {"$exists":true}}, {"$set": {"second test": "pass"}}, function(err, res){
+				assert.equal(null, err);
+				assert.equal(true, res.result.n >= 1);
+				assert.equal(true, res.result.nModified >= 1);
+				done();
+			})
+		})
+	});
+	describe('#delete', function(){
+		it('should remove according to a specific mongo query', function(done){
+			db.delete({}, function(err, res){
+				assert.equal(null, err);
+				assert.equal(true, res.result.n >= 1);
+				done();
+			})
+		})
+	})
+});
