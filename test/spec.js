06e510cb9592969dfe651608f6e937fe46d86e7a
molinarikris <kamolinari42@gmail.com>
2015-09-08 23:41:52 -0400

    Added aggregate method for pipelines. Will add more docs soon.

--- test/spec.js
+++ test/spec.js
@@ -5,14 +5,14 @@ var db = new dbConstructor('test', 'controller');
 describe('Mongode', function(){
 	it('should construct a wrapper with two parameters and an options object', function() {
 		var dummy = new dbConstructor('test', 'controller', {});
-		assert.equal(6, Object.keys(dummy).length);
+		assert.equal(7, Object.keys(dummy).length);
 	});
 	it('should construct a wrapper with one object containing the dbName and colName', function(){
 		var dummy2 = new dbConstructor({
 			dbName: 'test',
 			colName: 'controller'
 		});
-		assert.equal(6, Object.keys(dummy2).length);
+		assert.equal(7, Object.keys(dummy2).length);
 	});
 	it('should throw an error when neither occurence happens', function(){
 		assert.throws(function() {
@@ -24,7 +24,8 @@ describe('Mongode', function(){
 				should: "throw an error"
 			});
 		})
-	})
+	});
+
 	describe('#connection', function() {
 		it('should connect without error and return non-empty database object', function(done) {
 			db.connection(function(err, db){
@@ -46,6 +47,7 @@ describe('Mongode', function(){
 			})
 		})
 	});
+
 	describe('#create', function(){
 		it('should make a new database entry with no error', function(done){
 			db.create({"test": "pass", "a": 1}, function(err, res){
@@ -62,16 +64,15 @@ describe('Mongode', function(){
 			});
 		});
 		it('should be able to insert a document without specifying a callback', function(done){
-			db.create([{"hi":"test"}, {"hi":"test"}]);
-			setTimeout(function() {
-				db.read({"hi":"test"}, function(err, docs){
-					assert.equal(null, err);
-					assert.equal(2, docs.length);
-					done();
-				})
-			}, 25);
+			var promise = db.create([{"test":"test"}, {"test":"test"}]);
+			promise(function(res) {
+				assert.equal(res.result.ok, 1);
+				assert.equal(res.result.n, 2);
+				done();
+			})
 		})
 	});
+
 	describe('#read', function(){
 		it('should create an array of all database entries given no query', function(done){
 			db.read(function(err, docs){
@@ -88,6 +89,7 @@ describe('Mongode', function(){
 			})
 		})
 	});
+
 	describe('#update', function() {
 		it('should find a db entry and modify it without error', function(done){
 			db.update({"test": {$exists:true}}, {"$set": {"second test": "pass"}}, function(err, res){
@@ -98,7 +100,31 @@ describe('Mongode', function(){
 			})
 		})
 	});
+
+	describe('#aggregate', function() {
+		it('should run pipeline-based stats on a collection', function(done) {
+			db.aggregate({
+				$group: {
+					_id: "$test",
+					frequency: {$sum: 1}
+				}
+			}, {}, function(err, res) {
+				assert.equal(err, null);
+				assert.equal(res[0].frequency, 2);
+				assert.equal(res[0]._id, "pass");
+				done();
+			})
+		})
+	});
+
 	describe('#delete', function(){
+		it('should remove according to a mongo query.', function(done) {
+			db.delete({"a": 1}, function(err, res) {
+				assert.equal(null, err);
+				assert.equal(true, res.result.n >= 1);
+				done();
+			});
+		});
 		it('should remove everything according to an empty mongo query', function(done){
 			db.delete({}, function(err, res){
 				assert.equal(null, err);
@@ -107,4 +133,5 @@ describe('Mongode', function(){
 			})
 		})
 	})
+
 });
